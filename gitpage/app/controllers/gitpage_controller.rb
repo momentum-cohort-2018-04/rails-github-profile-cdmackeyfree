c0fe37e5a714fa5c97fd653456d2ab316650317f
Crystal Free <cdmackeyfree@gmail.com>
2018-06-20 09:03:55 -0400

    actually showing data from github

--- gitpage/app/controllers/gitpage_controller.rb
+++
@@ -1,6 +0,0 @@
-class GitpageController < ApplicationController
-  def index
-    @profile = HTTParty.get('https://api.github.com/search/users?q=cdmackeyfree')
-    @repos = HTTParty.get(https://api.github.com/users/cdmackeyfree/repos?sort=updated)
-  end
-end
